097f66aac33ef64b92e48a202819eeba1f26cadf
Aman Rakesh <amanrakesh046@gmail.com>
2021-01-02 22:01:18 +0530

    Update settings.py

--- MyCraigslist/MyCraigslist/settings.py
+++ MyCraigslist/MyCraigslist/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '58onjk=n^b#2dg^)=$l@xz45@(i=7)r3y+swt32k4$-ek7mt-b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
